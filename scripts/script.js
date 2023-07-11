d881c2728bc4179cdf686e8fead14a260d4cd5e4
Jadon Lama <Jadonlama357@gmail.com>
2023-07-11 13:33:19 -0400

    Update
    Now tracks high scores and uses localstorage for them.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 let clicks = 0;
 let button = document.querySelector('button');
+let highScore = localStorage.getItem('highScore') || 0;
 
-function add(){
-    clicks++;
-    button.textContent = clicks;
+function add() {
+  clicks++;
+  button.textContent = clicks;
+
+  if (clicks > highScore) {
+    highScore = clicks;
+    localStorage.setItem('highScore', highScore);
+    updateHighScore(); // Update the high score display
+  }
 }
+
+// Update high score display
+function updateHighScore() {
+  const highScoreElement = document.getElementById('highScore');
+  highScoreElement.textContent = `High Score: ${highScore}`;
+}
+
+// Call the function to update high score when the page loads
+updateHighScore();
